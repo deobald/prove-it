b417314c8325a4a026382be3a6a9fb5018ae82fe
Steven Deobald <steven@deobald.ca>
2011-12-14 17:44:31 -0600

    intro 3

--- src/prove_it/core.clj
+++ src/prove_it/core.clj
@@ -9,3 +9,9 @@
                          ;;(- (exp 3 n) 1)
                          (- (exp 3 n) (exp 2 n))])]
     (map make-tri (range 1 20))))
+
+(defn intro-3-a []
+  (+ 1 (* 2 3 5 7)))
+
+(defn intro-3-b []
+  (+ 1 (* 2 5 11)))
