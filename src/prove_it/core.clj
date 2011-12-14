2d12aa0a7fc6bf3b51eb500afb3c676fc8623867
Steven Deobald <steven@deobald.ca>
2011-12-14 17:56:22 -0600

    intro 4

--- src/prove_it/core.clj
+++ src/prove_it/core.clj
@@ -4,6 +4,14 @@
 (defn exp [i e]
   (.pow (BigInteger. (str i)) e))
 
+;; ha! yes, really.
+(def factorial
+  (fn [n]
+    (loop [cnt n acc 1]
+      (if (zero? cnt)
+        acc
+        (recur (dec cnt) (* acc cnt))))))
+
 (defn intro-2 []
   (let [make-tri (fn [n] [n
                          ;;(- (exp 3 n) 1)
@@ -15,3 +23,9 @@
 
 (defn intro-3-b []
   (+ 1 (* 2 5 11)))
+
+(defn intro-4 []
+  (let [n 5
+        x (+ 2 (factorial (+ n 1)))]
+    (for [i (range 0 n)]
+      (+ x i))))
